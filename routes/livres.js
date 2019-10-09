305921bf4a61171dc69037287440000f96b8c5e6
Jeremy Poliquin <1356053@cstj.qc.ca>
2019-10-09 11:38:58 -0400

    Modification livres

--- routes/livres.js
+++ routes/livres.js
@@ -10,16 +10,18 @@ const Livre = mongoose.model('Livre');
 // Méthode permettant de sélectionner un livre en particulier par son __id
 router.get('/:uuidLivre', async (req,res,next) => {
     try {
-        let livreQuery = await Livre.findOne({_id: req.params.uuidLivre});
+        let livreQuery = Livre.find({_id: req.params.uuidLivre});
+        try {
+            let livres = await livreQuery;
+            if(livres.length == 0){
+                // Aucun livre à été trouvé... On retourne une erreur 404.¸
+                next(new createError.NotFound(`Le livre avec l'identifiant ${req.params.uuidLivre} n'existe pas.`));
+            }
 
-
-        let livre = await livreQuery;
-
-        if(livre.length == 0){
-            // Aucun livre à été trouvé... On retourne une erreur 404.
+            res.status(200).json(livres[0]);
+        } catch(err)
+        {
             next(new createError.NotFound(`Le livre avec l'identifiant ${req.params.uuidLivre} n'existe pas.`));
-        } else {
-            res.status(200).json(livre[0]);
         }
     } catch(err) {
         next(new createError.InternalServerError(err.message));
